d36655ac0adc973ad2e5e5299735cece616d4706
Arturo Castillo <Arturo Castillo>
2017-01-25 19:22:25 -0300

    cambio de titulo en index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <title>DSR|Arquitectura </title>
+    <title>Prueba </title>
     <link rel="shortcut icon" href="img/favicon.ico">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="css/estilos.css">
